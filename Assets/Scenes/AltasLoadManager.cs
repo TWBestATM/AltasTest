1c8814221d26d55fc5dae6d4d48aeec015e1aa5a
loneMan <as5405as@gamewolf.com>
2020-08-05 13:51:59 +0800

    update

---
+++ Assets/Scenes/AltasLoadManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D;
+
+public class AltasLoadManager : MonoBehaviour
+{
+    public static AltasLoadManager instance;
+    Dictionary<string, AsyncOperationHandle<SpriteAtlas>> atlasLoadedDic = new Dictionary<string, AsyncOperationHandle<SpriteAtlas>>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+
+        }
+        instance = this;
+        SpriteAtlasManager.atlasRequested+= RequestAtlas;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void RequestAtlas(string tag, System.Action<UnityEngine.U2D.SpriteAtlas> callback)
+    {
+        Debug.Log("load" + tag);
+        AsyncOperationHandle<SpriteAtlas> async = Addressables.LoadAssetAsync<SpriteAtlas>(tag);
+        async.Completed += (ob) => { callback(ob.Result); };
+        if (!atlasLoadedDic.ContainsKey(tag))
+        {
+            atlasLoadedDic.Add(tag, async);
+        }
+    }
+    public void releaseAltas(string tag)
+    {
+        if (atlasLoadedDic.ContainsKey(tag))
+        {
+            Addressables.Release(atlasLoadedDic[tag]);
+            Debug.Log("release" + tag);
+            atlasLoadedDic.Remove(tag);
+        }
+
+
+    }
+    protected void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+            SpriteAtlasManager.atlasRequested -= RequestAtlas;
+            Debug.Log("destory");
+        }
+    }
+}
---
+++ Assets/Scenes/altasLoadTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+public class altasLoadTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject loadingTip = null;
+    public void loadSceen()
+    {
+        Addressables.LoadSceneAsync("SampleScene", LoadSceneMode.Single, true);
+        loadingTip.SetActive(true);
+
+    }
+
+    public void backMain()
+    {
+        AltasLoadManager.instance.releaseAltas("test");
+        SceneManager.LoadScene("main");
+        loadingTip.SetActive(true);
+    }
+}
